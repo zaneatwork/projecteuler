6ce15c6367423ed0102c6390cfd0353676262c71
Zane <zane@kicksite.net>
2026-05-28 13:30:25 -0500

    fixed problem 2 in c

--- c/problem002.c
+++ c/problem002.c
@@ -4,16 +4,17 @@
 #include <stdio.h>
 
 int main() {
-  int x = 1, y = 2, sumOfEvens = 2;
+  int x = 1, y = 2;
+  unsigned long sumOfEvens = 2;
 
   while (y <= 4000000) {
     // Doing a little math for fun to avoid having a separate "sum" variable 
     // for calculating fibonacci :)
     y = x+y;
     x = y-x;
-    sumOfEvens += x+y % 2 ? 0 : x+y;
+    sumOfEvens += (x+y) % 2 ? 0 : x+y;
   }
 
-  printf("%d\n", sumOfEvens);
+  printf("%lu\n", sumOfEvens);
   return 0;
 }
